60aa1e1fbd00c0f45ce4355229a1d2dedf3d68a1
abdullah-ch <abdullahchaudharry@gmail.com>
2024-10-29 21:21:29 +0500

    Refactor fetchTitle function to use promises instead of callbacks

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const http = require('http');
 const https = require('https');
-const async = require('async');
+const Q = require('q');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5001;
 
-function fetchTitle(address, callback) {
+function fetchTitle(address) {
+    const deferred = Q.defer(); 
     const formattedAddress = address.startsWith('http') ? address : `http://${address}`;
     const protocol = formattedAddress.startsWith('https') ? https : http;
 
@@ -16,36 +17,38 @@ function fetchTitle(address, callback) {
         res.on('end', () => {
             const titleMatch = data.match(/<title>(.*?)<\/title>/i);
             const title = titleMatch ? titleMatch[1].trim() : 'NO RESPONSE';
-            callback(null, `<li>${address} - "${title}"</li>`);
+            deferred.resolve(`<li>${address} - "${title}"</li>`); // Resolve the promise
         });
     }).on('error', () => {
-        callback(null, `<li>${address} - NO RESPONSE</li>`);
+        deferred.resolve(`<li>${address} - NO RESPONSE</li>`); // Resolve with an error message
     });
+
+    return deferred.promise; // Return the promise
 }
 
 app.get('/I/want/title', (req, res) => {
     const addresses = Array.isArray(req.query.address) ? req.query.address : [req.query.address];
     
-    async.map(addresses, fetchTitle, (err, results) => {
-        if (err) {
-            return res.status(500).send('An error occurred while fetching titles');
-        }
-        
-        const responseHtml = `
-            <html>
-                <head></head>
-                <body>
-                    <h1>Following are the titles of given websites:</h1>
-                    <ul>${results.join('')}</ul>
-                </body>
-            </html>`;
-        
-        res.send(responseHtml);
-    });
+    // Map each address to a fetchTitle promise
+    Q.all(addresses.map(fetchTitle))
+        .then(results => {
+            const responseHtml = `
+                <html>
+                    <head></head>
+                    <body>
+                        <h1>Following are the titles of given websites:</h1>
+                        <ul>${results.join('')}</ul>
+                    </body>
+                </html>`;
+            res.send(responseHtml);
+        })
+        .catch(() => {
+            res.status(500).send('An error occurred while fetching titles');
+        });
 });
 
 app.use((req, res) => {
-    return res.status(404).send("404 Not Found");
+    res.status(404).send("404 Not Found");
 });
 
 app.listen(port, () => {
