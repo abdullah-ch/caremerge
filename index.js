e9c467a82a014566adeedf30876df0e5b1fa5ae6
abdullah-ch <abdullahchaudharry@gmail.com>
2024-10-29 21:04:49 +0500

    Refactor server setup and add functionality to fetch website titles

--- index.js
+++ index.js
@@ -1,53 +1,47 @@
 const express = require('express');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
 const http = require('http');
 const https = require('https');
-const app = express();
 
+const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5001;
 
-app.use(
-  cors({
-    credentials: true,
-    origin: true,
-  })
-);
-app.use(express.json());
-app.use(cookieParser());
+function fetchTitle(address) {
+    return new Promise((resolve) => {
+        // Add a default protocol if not present
+        const formattedAddress = address.startsWith('http') ? address : `http://${address}`;
+        const protocol = formattedAddress.startsWith('https') ? https : http;
 
-function fetchTitle(address, callback) {
-    // Add a default protocol if not present
-    const formattedAddress = address.startsWith('http') ? address : `http://${address}`;
-    const protocol = formattedAddress.startsWith('https') ? https : http;
-
-    protocol.get(formattedAddress, (res) => {
-        let data = '';
-        res.on('data', chunk => data += chunk);
-        res.on('end', () => {
-            const titleMatch = data.match(/<title>(.*?)<\/title>/i);
-            callback(null, `<li>${address} - "${titleMatch ? titleMatch[1] : 'NO RESPONSE'}"</li>`);
+        protocol.get(formattedAddress, (res) => {
+            let data = '';
+            res.on('data', chunk => data += chunk);
+            res.on('end', () => {
+                const titleMatch = data.match(/<title>(.*?)<\/title>/i);
+                const title = titleMatch ? titleMatch[1].trim() : 'NO RESPONSE';
+                resolve(`<li>${address} - "${title}"</li>`);
+            });
+        }).on('error', () => {
+            resolve(`<li>${address} - NO RESPONSE</li>`);
         });
-    }).on('error', () => {
-        callback(null, `<li>${address} - NO RESPONSE</li>`);
     });
 }
 
-app.get('/I/want/title', (req, res) => {
+app.get('/I/want/title', async (req, res) => {
     const addresses = Array.isArray(req.query.address) ? req.query.address : [req.query.address];
-    let titles = [];
-    let count = 0;
-
-    addresses.forEach((address, index) => {
-        fetchTitle(address, (err, title) => {
-            titles[index] = `<li>${address} - "${title}"</li>`;
-            count++;
-            if (count === addresses.length) {
-                res.send(`<html><body><h1>Following are the titles of given websites:</h1><ul>${titles.join('')}</ul></body></html>`);
-            }
-        });
-    });
+    
+    const results = await Promise.all(addresses.map(fetchTitle));
+    
+    // Construct the response HTML
+    const responseHtml = `
+        <html>
+            <head></head>
+            <body>
+                <h1>Following are the titles of given websites:</h1>
+                <ul>${results.join('')}</ul>
+            </body>
+        </html>`;
+    
+    res.send(responseHtml);
 });
 
 app.use((req, res) => {
